d7712cfbf61b3c342c4279113ec9bde5854e60d0
Pavol Kišon <pavol.kison@gmail.com>
2011-10-26 16:57:28 +0200

    common_defs: DTYPE_t

--- scikits/sundials_odes/common_defs.pxd
+++ scikits/sundials_odes/common_defs.pxd
@@ -1,21 +1,23 @@
 cimport numpy as np
 from c_sundials cimport N_Vector
 
+ctypedef np.float_t DTYPE_t
+
 cdef class ResFunction:
-    cpdef int evaluate(self, float t, 
-                       np.ndarray[float, ndim=1] y,
-                       np.ndarray[float, ndim=1] ydot,
-                       np.ndarray[float, ndim=1] result)
+    cpdef int evaluate(self, DTYPE_t t, 
+                       np.ndarray[DTYPE_t, ndim=1] y,
+                       np.ndarray[DTYPE_t, ndim=1] ydot,
+                       np.ndarray[DTYPE_t, ndim=1] result)
         
 cdef class JacFunction:
-    cpdef np.ndarray[float, ndim=2] evaluate(self, float t, 
-                                             np.ndarray[float, ndim=1] y,
-                                             np.ndarray[float, ndim=1] ydot,
-                                             float cj,
-                                             np.ndarray[float, ndim=2] J)
+    cpdef np.ndarray[DTYPE_t, ndim=2] evaluate(self, DTYPE_t t, 
+                                             np.ndarray[DTYPE_t, ndim=1] y,
+                                             np.ndarray[DTYPE_t, ndim=1] ydot,
+                                             DTYPE_t cj,
+                                             np.ndarray[DTYPE_t, ndim=2] J)
 
 
-cdef inline int nv_s2ndarray(N_Vector v, np.ndarray[float, ndim=1] a)
+cdef inline int nv_s2ndarray(N_Vector v, np.ndarray[DTYPE_t, ndim=1] a)
       
-cdef inline int ndarray2nv_s(N_Vector v, np.ndarray[float, ndim=1] a)
+cdef inline int ndarray2nv_s(N_Vector v, np.ndarray[DTYPE_t, ndim=1] a)
     
--- scikits/sundials_odes/common_defs.pyx
+++ scikits/sundials_odes/common_defs.pyx
@@ -3,29 +3,30 @@ cimport numpy as np
 from c_sundials cimport N_Vector, nv_content_data_s, nv_content_s, nv_length_s, nv_data_s, get_nv_ith_s, set_nv_ith_s
 
 cdef class ResFunction:
-    cpdef int evaluate(self, float t, 
-                       np.ndarray[float, ndim=1] y,
-                       np.ndarray[float, ndim=1] ydot,
-                       #void *userdata = NULL
-                       np.ndarray[float, ndim=1] result):
-        return np.array([])
+    cpdef int evaluate(self, DTYPE_t t, 
+                       np.ndarray[DTYPE_t, ndim=1] y,
+                       np.ndarray[DTYPE_t, ndim=1] ydot,
+                      #void *userdata = NULL
+                       np.ndarray[DTYPE_t, ndim=1] result):
+        return 0
         
 cdef class JacFunction:
-    cpdef np.ndarray[float, ndim=2]  evaluate(self, float t, 
-                                             np.ndarray[float, ndim=1] y,
-                                             np.ndarray[float, ndim=1] ydot,
-                                             float cj,
-                                             np.ndarray[float, ndim=2] J):
+    cpdef np.ndarray[DTYPE_t, ndim=2]  evaluate(self, DTYPE_t t, 
+                                             np.ndarray[DTYPE_t, ndim=1] y,
+                                             np.ndarray[DTYPE_t, ndim=1] ydot,
+                                             DTYPE_t cj,
+                                             np.ndarray[DTYPE_t, ndim=2] J):
         """
-            Returns the Jacobi matrix (for dense the full matrix, for band only
-            bands. Result has to be stored in the variable J, which is preallocated
-            to the corresponding size.
+        Returns the Jacobi matrix (for dense the full matrix, for band only
+        bands. Result has to be stored in the variable J, which is preallocated
+        to the corresponding size.
+            
+        This is a generic class, you should subclass is for the problem specific
+        purposes."
         """
-        raise NotImplemented("You should subclass this generic class for specific\
-                              purposes of the problem.")
         return 0
 
-cdef inline int nv_s2ndarray(N_Vector v, np.ndarray[float, ndim=1] a):
+cdef inline int nv_s2ndarray(N_Vector v, np.ndarray[DTYPE_t, ndim=1] a):
     cdef unsigned int N, i
     N = nv_length_s(nv_content_s(v))
     cdef nv_content_data_s v_data = nv_data_s(nv_content_s(v))
@@ -33,7 +34,7 @@ cdef inline int nv_s2ndarray(N_Vector v, np.ndarray[float, ndim=1] a):
     for i in range(N):
       a[i] = get_nv_ith_s(v_data, i)
       
-cdef inline int ndarray2nv_s(N_Vector v, np.ndarray[float, ndim=1] a):
+cdef inline int ndarray2nv_s(N_Vector v, np.ndarray[DTYPE_t, ndim=1] a):
     cdef unsigned int N, i
     N = nv_length_s(nv_content_s(v))
     cdef nv_content_data_s v_data = nv_data_s(nv_content_s(v))
