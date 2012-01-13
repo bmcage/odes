50c931c7aaa1c65329c3de8e42a2d2afafc36555
Pavol Kišon <pavol.kison@gmail.com>
2012-01-13 12:31:19 +0100

    IDA: format & tweak (part 1) [add new options, rename some old, and add more options tests;  add _init_step and _solve methods]

--- scikits/odes/sundials/ida.pyx
+++ scikits/odes/sundials/ida.pyx
@@ -10,7 +10,6 @@ from common_defs cimport (nv_s2ndarray, ndarray2nv_s, ensure_numpy_float_array,
 # TODO: parallel implementation: N_VectorParallel
 # TODO: linsolvers: check the output value for errors
 # TODO: flag for indicating the resfn (in set_options) whether is a c-function or python function
-# TODO: implement event handler
 # TODO: unify using float/double/realtype variable
 # TODO: optimize code for compiler
 
@@ -35,12 +34,12 @@ cdef int _res(realtype tt, N_Vector yy, N_Vector yp,
         nv_s2ndarray(yp, yp_tmp)
          
     aux_data.res.evaluate(tt, yy_tmp, yp_tmp, residual_tmp, aux_data.user_data)
-         
+
     if parallel_implementation:
         raise NotImplemented 
     else:
         ndarray2nv_s(rr, residual_tmp)
-         
+
     return 0
 
 cdef int _jacdense(int Neq, realtype tt, realtype cj, 
@@ -83,26 +82,32 @@ cdef class IDA_data:
         self.yp_tmp = np.empty(N, float)
         self.residual_tmp = np.empty(N, float)
         self.jac_tmp = None
-    
+
 cdef class IDA:
 
-    def __cinit__(self, residualfn, **options):
-        """ Create the IDA Solver and initialize default values """
-        
-        self._ida_mem = IDACreate()
+    def __cinit__(self, Rfn, **options):
+        """
+        Initialize the IDA Solver and it's default values
+
+        Input:
+            Rfn     - residual function
+            options - additional options for initialization
+        """
         
         default_values = {
             'implementation': 'serial',
-            'use_relaxation': False,
             'rtol': 1e-6, 'atol': 1e-12,
             'linsolver': 'dense',
             'lband': 0,'uband': 0,
             'maxl': 0,
             'tcrit': 0.,
-            'order': 5,
-            'nsteps':500,
-            'max_step': 0.,
-            'first_step': 0.,
+            'order': 0,
+            'max_step_size': 0.,
+            'first_step_size': 0.,
+            'max_steps': 0,
+            'max_conv_fails': 0,
+            'max_nonlin_iters': 0,
+            'nonlin_conv_coef': 0.,
             'compute_initcond': None,
             'compute_initcond_t0': 0.01,
             'constraints_idx': None,
@@ -110,14 +115,14 @@ cdef class IDA:
             'algebraic_vars_idx':None, 
             'exclude_algvar_from_error': False,
             'user_data': None,
-            'out': False, #,
-            'resfn': None,
+            'rfn': None,
             'jacfn': None
             }
  
         self.options = default_values
         self.N       = -1
-        self.set_options(resfn=residualfn, **options)
+        self.set_options(rfn=Rfn, **options)
+        self._ida_mem = NULL
         self.initialized = False
 
     def set_options(self, **options):
@@ -171,16 +176,16 @@ cdef class IDA:
                 Values: 1, 2, 3, 4, 5 (= default)
                 Description:
                     Specifies the maximum order of the linear multistep method.
-            'nsteps':
-                Values: positive integer, 500 = default
+            'max_steps':
+                Values: positive integer, 0 = default (uses value of 500)
                 Description:
                     Maximum number of (internally defined) steps allowed during one call to the solver.
-            'max_step': 
+            'max_step_size':
                 Values: non-negative float, 0.0 = default
                 Description:
                     Restricts the maximal (absolute) internal step value taken by the solver. The value of 0.0 uses
                     no restriction on the maximal step size.
-            'first_step': 
+            'first_step_size':
                 Values: float , 0.0 = default
                 Description:
                     Sets the first step size. DAE solver can suffer on the first step, so set this
@@ -244,18 +249,40 @@ cdef class IDA:
                              2.0 - variable has to be positive (i.e. > 0)
                             -1.0 - variable has to be non-positive (i.e. <= 0)
                             -2.0 - variable has to be negative (i.e. < 0)
-            'out': False, #
         """
 
         for (key, value) in options.items():
             self.options[key.lower()] = value
         self.initialized = False
-            
-    cpdef init_step(self, DTYPE_t t0, 
-                    np.ndarray[DTYPE_t, ndim=1] y0, 
-                    np.ndarray[DTYPE_t, ndim=1] yp0,
-                    np.ndarray y_ic0_retn = None,
-                    np.ndarray yp_ic0_retn = None):
+
+     def init_step(self, double t0, object y0, object yp0,
+                   np.ndarray y_ic0_retn = None,
+                   np.ndarray yp_ic0_retn = None):
+        """
+        Initializes the solver and allocates memory.
+
+        Input:
+            t0     - initial time
+            y0     - initial condition for y (can be list or numpy array)
+            yp0    - initial condition for yp (can be list or numpy array)
+            y_ic0  - (optional) returns the calculated consistent initial condition for y
+                     It MUST be a numpy array.
+            yp_ic0 - (optional) returns the calculated consistent initial
+                     condition for y derivated. It MUST be a numpy array.
+        """
+
+        cdef np.ndarray[DTYPE_t, ndim=1] np_y0
+        cdef np.ndarray[DTYPE_t, ndim=1] np_yp0
+        np_y0  = np.asarray(y0)
+        np_yp0 = np.asarray(yp0)
+
+        return self._init_step(t0, np_y0, np_yp0, y_ic0_retn, yp_ic0_retn)
+
+    cpdef _init_step(self, DTYPE_t t0, 
+                     np.ndarray[DTYPE_t, ndim=1] y0,
+                     np.ndarray[DTYPE_t, ndim=1] yp0,
+                     np.ndarray y_ic0_retn = None,
+                     np.ndarray yp_ic0_retn = None):
         """ 
             Applies the set by 'set_options' method to the IDA solver.
             
@@ -277,16 +304,17 @@ cdef class IDA:
             Returns the time of the intial step.
         """
         #TODO: jacobi function ?isset
-        #TODO: implement relaxation algoithm for opt: opts['use_relaxation']
 
-        if self._ida_mem is NULL:
-            raise MemoryError
- 
         cdef dict opts = self.options
+
         self.parallel_implementation = (opts['implementation'].lower() == 'parallel')
-     
-        if opts['resfn'] == None:
-            raise ValueError('The residual function ResFn not assigned '
+        if self.parallel_implementation:
+            raise ValueError('Error: Parallel implementation not implemented !')
+        cdef long int N
+        N = <long int> np.alen(y0)
+
+        if opts['rfn'] == None:
+            raise ValueError('The residual function rfn not assigned '
                               'during ''set_options'' call !')
                
         if not np.alen(y0) == np.alen(yp0):
@@ -296,10 +324,8 @@ cdef class IDA:
                 or ((np.alen(yp0) == 0) and (not opts['compute_initcond'] == 'yp0'))):
             raise ValueError('Not passed y0 or ydot0 value has to computed'
                              'by ''init_cond'', but ''init_cond'' not set apropriately !')
-        cdef long int N
-        N = <long int> np.alen(y0)
+ 
         
-        cdef void* ida_mem = self._ida_mem
 
         #TODO: when implementing parallel, does N_VDestroy be called separately
         #      for parallel version or it's a generic one?
@@ -309,131 +335,198 @@ cdef class IDA:
             N_VDestroy(self.residual)
             N_VDestroy(self.y)
             N_VDestroy(self.yp)
- 
+
         if self.parallel_implementation:
             raise NotImplemented
         else:
             self.y0  = N_VMake_Serial(N, <realtype *>y0.data)
             self.yp0 = N_VMake_Serial(N, <realtype *>yp0.data)
             self.residual = N_VNew_Serial(N)
-            self.y  = N_VClone(self.y0)
-            self.yp = N_VClone(self.yp0)
- 
-        
-        if self.N <= 0:
-            IDAInit(ida_mem, _res, <realtype> t0, self.y0, self.yp0)
+            self.y   = N_VClone(self.y0)
+            self.yp  = N_VClone(self.yp0)
+
+        cdef int flag
+        cdef void* ida_mem = self._ida_mem
+
+        if (ida_mem is NULL) or (self.N != N):
+            if (not ida_mem is NULL):
+                IDAFree(&ida_mem)
+            ida_mem = IDACreate()
+            if ida_mem is NULL:
+                raise MemoryError('IDACreate:MemoryError: Could not create ida_mem object')
+
+            self._ida_mem = ida_mem
+            flag = IDAInit(ida_mem, _res, <realtype> t0, self.y0, self.yp0)
         elif self.N == N:
-            IDAReInit(ida_mem, <realtype> t0, self.y0, self.yp0)
+            flag = IDAReInit(ida_mem, <realtype> t0, self.y0, self.yp0)
         else:
-            # we should completely reallocate ida_mem, but...
-            # probably also the res and jas functions should be changed
-            # ==> add them to the (possible) options (set_options(...))
-            raise NotImplemented('IDA object reallocation not implemented !')
-        
+            raise ValueError('IDAInit:Error: You should not be here...')
+        if flag == IDA_ILL_INPUT:
+                raise ValueError('IDA[Re]Init: Ill input')
+        elif flag == IDA_MEM_FAIL:
+            raise MemoryError('IDA[Re]Init: Memory allocation error')
+        elif flag == IDA_MEM_NULL:
+            raise MemoryError('IDACreate: Memory allocation error')
+        elif flag == IDA_NO_MALLOC:
+            raise MemoryError('IDAReInit: No memory allocated in IDAInit.')
+
         self.N = N
-        
+
         # auxiliary variables
         self.aux_data = IDA_data(N)
         self.aux_data.parallel_implementation = self.parallel_implementation
 
-        if not isinstance(opts['resfn'] , ResFunction):
+        rfn = opts['rfn']
+        if not isinstance(rfn , ResFunction):
             tmpfun = WrapResFunction()
-            tmpfun.set_resfn(opts['resfn'])
-            opts['resfn'] = tmpfun
-        self.aux_data.res = opts['resfn']
+            tmpfun.set_resfn(rfn)
+            rfn = tmpfun
+            opts['rfn'] = tmpfun
+        self.aux_data.res = rfn
         self.aux_data.jac = opts['jacfn']
         self.aux_data.user_data = opts['user_data']
-        
+
         if not np.isscalar(opts['rtol']) :
-            raise ValueError('rtol (%s) must be a scalar for IDA'\
+            raise ValueError('rtol (%s) must be a scalar for IDA'
                                 % (opts['rtol']))
+
         cdef N_Vector atol
         cdef np.ndarray[DTYPE_t, ndim=1] np_atol
-        cdef int maxl = <int> opts['maxl']
+
         if not (self.atol is NULL):
             N_VDestroy(self.atol)
             self.atol = NULL
         if np.isscalar(opts['atol']):
-            IDASStolerances(ida_mem, <realtype> opts['rtol'], <realtype> opts['atol'])
+            flag = IDASStolerances(ida_mem, <realtype> opts['rtol'],
+                                            <realtype> opts['atol'])
         else:
             np_atol = np.asarray(opts['atol'])
             if self.parallel_implementation:
                 raise NotImplemented
             else:
                 atol = N_VMake_Serial(N, <realtype *> np_atol.data)
-            IDASVtolerances(ida_mem, <realtype> opts['rtol'], atol)
+            flag = IDASVtolerances(ida_mem, <realtype> opts['rtol'], atol)
         #TODO: implement IDAFWtolerances(ida_mem, efun)
         
+        if flag == IDA_ILL_INPUT:
+            raise ValueError('IDAStolerances: negative atol or rtol value.')
+
         # As user data we pass the IDA_data object
         IDASetUserData(ida_mem, <void*> self.aux_data)
-        
-        cdef int order = <int> opts['order']
-        if (order < 1) or (order > 5):
-            raise ValueError('order should be betwen 1 and 5')
-        IDASetMaxOrd(ida_mem, order)
-        IDASetMaxNumSteps(ida_mem, <int> opts['nsteps'])
-        if opts['max_step'] > 0.:
-            IDASetMaxStep(ida_mem, <int> opts['max_step'])
+
+        if (opts['order'] > 0):
+            IDASetMaxOrd(cv_mem, <int> opts['order'])
+        IDASetMaxNumSteps(ida_mem, <int> opts['max_steps'])
+        if opts['first_step_size'] > 0.:
+            IDASetInitStep(ida_mem, <realtype> opts['first_step_size'])
+        if opts['max_step_size'] > 0.:
+            flag = IDASetMaxStep(ida_mem, <realtype> opts['max_step_size'])
+            if flag == IDA_ILL_INPUT:
+                raise ValueError('IDASetMaxStep: max_step_size is negative or smaller than allowed.')
         if opts['tcrit'] > 0.:
             IDASetStopTime(ida_mem, <realtype> opts['tcrit'])
-        if opts['first_step'] > 0.:
-            IDASetInitStep(ida_mem, <realtype> opts['first_step'])
-        #TODO: implement the rest of IDASet* functions
-        
+        if opts['max_nonlin_iters'] > 0:
+            IDASetMaxNonlinIters(ida_mem, <int> opts['max_nonlin_iters'])
+        if opts['max_conv_fails'] > 0:
+            IDASetMaxConvFails(ida_mem, <int> opts['max_conv_fails'])
+        if opts['nonlin_conv_coef'] > 0:
+            IDASetNonlinConvCoef(ida_mem, <int> opts['nonlin_conv_coef'])
+
+        # Linsolver
         linsolver = opts['linsolver'].lower()
+
         if linsolver == 'dense':
             if self.parallel_implementation:
                 raise ValueError('Linear solver for dense matrices can be used'
                                   'only for serial implementation. Use ''lapackdense''' 
                                   'instead for parallel implementation.')
             else:
-                 IDADense(ida_mem, N)
+                 flag = IDADense(ida_mem, N)
+                 if flag == IDADLS_ILL_INPUT:
+                        raise ValueError('IDADense solver is not compatible with'
+                                         ' the current nvector implementation.')
+                    elif flag == IDADLS_MEM_FAIL:
+                        raise MemoryError('IDADense memory allocation error.')
         elif linsolver == 'lapackdense':
-            IDALapackDense(ida_mem, N)
+            flag = IDALapackDense(ida_mem, N)
+            if flag == IDADLS_ILL_INPUT:
+                    raise ValueError('IDALapackDense solver is not compatible with'
+                                         ' the current nvector implementation.')
+                elif flag == IDADLS_MEM_FAIL:
+                    raise MemoryError('IDALapackDense memory allocation error.')
         elif linsolver == 'band':
             if self.parallel_implementation:
                 raise ValueError('Linear solver for band matrices can be used'
-                                  'only for serial implementation. Use ''lapackband'' '
-                                  'instead for parallel implementation.')
+                                 'only for serial implementation. Use ''lapackband'' '
+                                 'instead for parallel implementation.')
             else:
-                pass
-                IDABand(ida_mem, N, <int> opts['uband'], <int> opts['lband']) 
+                flag = IDABand(ida_mem, N, <int> opts['uband'],
+                                           <int> opts['lband'])
+                if flag == IDADLS_ILL_INPUT:
+                    raise ValueError('IDABand solver is not compatible'
+                                     ' with the current nvector implementation'
+                                     ' or bandwith outside range.')
+                elif flag == IDADLS_MEM_FAIL:
+                    raise MemoryError('IDABand memory allocation error.')
         elif linsolver == 'lapackband':
-            IDALapackBand(ida_mem, N, <int> opts['uband'], <int> opts['lband']) 
-        elif linsolver == 'spgmr':
-            IDASpgmr(ida_mem, maxl)
-        elif linsolver == 'spbcg':
-            IDASpbcg(ida_mem, maxl)
-        elif linsolver == 'sptfqmr':
-            IDASptfqmr(ida_mem, maxl)
+            flag = IDALapackBand(ida_mem, N, <int> opts['uband'],
+                                             <int> opts['lband'])
+            if flag == IDADLS_ILL_INPUT:
+                raise ValueError('IDALapackBand solver is not compatible'
+                                 ' with the current nvector implementation'
+                                 ' or bandwith outside range.')
+            elif flag == IDADLS_MEM_FAIL:
+                raise MemoryError('IDALapackBand memory allocation error.')
+        elif ((linsolver == 'spgmr') or (linsolver == 'spbcg')
+                  or (linsolver == 'sptfqmr')):
+            maxl = <int> opts['maxl']
+
+            if linsolver == 'spgmr':
+                flag = IDASpgmr(ida_mem, maxl)
+            elif linsolver == 'spbcg':
+                flag = IDASpbcg(ida_mem, maxl)
+            elif linsolver == 'sptfqmr':
+                flag = IDASptfqmr(ida_mem, maxl)
+
+            if flag == IDASPILS_MEM_FAIL:
+                raise MemoryError('LinSolver:IDASpils memory allocation error.')
         
         if (linsolver in ['dense', 'lapackdense']) and self.aux_data.jac:
             IDADlsSetDenseJacFn(ida_mem, _jacdense)
 
-        constraints_idx = opts['constraints_idx']
+        # Constraints
+        constraints_idx  = opts['constraints_idx']
         constraints_type = opts['constraints_type']
         cdef unsigned int idx
         cdef np.ndarray[DTYPE_t, ndim=1] constraints_vars
+
         if not constraints_type is None:
             if not constraints_idx is None:
                 constraints_vars = np.zeros(N, float)
                 for idx in range(constraints_idx):
                     constraints_vars[constraints_idx[idx]] = constraints_type[idx]
             else:
-                assert np.alen(constraints_type) == N, 'Without ''constraints_idx'' specified the ''constraints_type'' has to be of the same length as y.'
+                assert np.alen(constraints_type) == N, 'Without ''constraints_idx'' specified the ''constraints_type'' the constraints_idx has to be of the same length as y (i.e. contains constraints for EVERY component of y).'
                 constraints_vars = np.asarray(constraints_type, float)
+
             if not self.constraints is NULL:
                 N_VDestroy(self.constraints)
+                self.constraints = NULL
+
             if self.parallel_implementation:
                 raise NotImplemented
             else:   
                 self.constraints = N_VMake_Serial(N, <realtype*> constraints_vars.data)
-            IDASetConstraints(ida_mem, self.constraints)
- 
+            flag = IDASetConstraints(ida_mem, self.constraints)
+            if flag == IDA_ILL_INPUT:
+                raise ValueError('IDAConstraints: ''constraints_type'' contains illegal value.')
+
+        # DAE variables and Initial condition
         cdef np.ndarray[DTYPE_t, ndim=1] dae_vars
         alg_vars_idx = opts['algebraic_vars_idx']
         compute_initcond = opts['compute_initcond']
-        if (compute_initcond == 'yp0') or (opts['exclude_algvar_from_error'] and not alg_vars_idx is None):
+        if ((compute_initcond == 'yp0') or
+            (opts['exclude_algvar_from_error'] and not alg_vars_idx is None)):
             dae_vars = np.ones(N, float)
             if not alg_vars_idx is None:
                 for algvar_idx in opts['algebraic_vars_idx']:
