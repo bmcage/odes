76e72daad074d89561dfa042ec2f1043f0d116b5
Pavol Kišon <pavol.kison@gmail.com>
2012-05-24 09:47:51 +0200

    cvode: string formatting and removing spaces

--- scikits/odes/sundials/cvode.pyx
+++ scikits/odes/sundials/cvode.pyx
@@ -10,7 +10,8 @@ from common_defs cimport (nv_s2ndarray, ndarray2nv_s,
 
 # TODO: parallel implementation: N_VectorParallel
 # TODO: linsolvers: check the output value for errors
-# TODO: flag for indicating the rhsfn (in set_options) whether is a c-function or python function
+# TODO: flag for indicating the rhsfn (in set_options) whether is
+#       a c-function or python function
 # TODO: unify using float/double/realtype variable
 # TODO: optimize code for compiler
 
@@ -20,51 +21,51 @@ cdef int _rhsfn(realtype tt, N_Vector yy, N_Vector yp,
               void *auxiliary_data):
     """function with the signature of CVRhsFn, that calls python Rhs"""
     cdef np.ndarray[DTYPE_t, ndim=1] yy_tmp, yp_tmp
-    
+
     aux_data = <CV_data> auxiliary_data
     cdef bint parallel_implementation = aux_data.parallel_implementation
 
     if parallel_implementation:
-        raise NotImplemented 
+        raise NotImplemented
     else:
         yy_tmp = aux_data.yy_tmp
         yp_tmp = aux_data.yp_tmp
-             
+
         nv_s2ndarray(yy, yy_tmp)
         #nv_s2ndarray(yp, yp_tmp)
-         
+
     aux_data.rfn.evaluate(tt, yy_tmp, yp_tmp, aux_data.user_data)
 
     if parallel_implementation:
-        raise NotImplemented 
+        raise NotImplemented
     else:
         ndarray2nv_s(yp, yp_tmp)
 
     return 0
 
-cdef int _jacdense(int Neq, realtype tt, 
-            N_Vector yy, N_Vector ff, DlsMat Jac, 
+cdef int _jacdense(int Neq, realtype tt,
+            N_Vector yy, N_Vector ff, DlsMat Jac,
             void *auxiliary_data, N_Vector tmp1, N_Vector tmp2, N_Vector tmp3):
     """function with the signature of CVDlsDenseJacFn that calls python Jac"""
     cdef np.ndarray[DTYPE_t, ndim=1] yy_tmp
     cdef np.ndarray jac_tmp
-    
+
     aux_data = <CV_data> auxiliary_data
     cdef bint parallel_implementation = aux_data.parallel_implementation
     if parallel_implementation:
-        raise NotImplemented 
+        raise NotImplemented
     else:
         yy_tmp = aux_data.yy_tmp
         if aux_data.jac_tmp == None:
             N = np.alen(yy_tmp)
             aux_data.jac_tmp = np.empty((N,N), float)
         jac_tmp = aux_data.jac_tmp
-             
+
         nv_s2ndarray(yy, yy_tmp)
     aux_data.jac.evaluate(tt, yy_tmp, jac_tmp)
 
     if parallel_implementation:
-        raise NotImplemented 
+        raise NotImplemented
     else:
         #we convert the python jac_tmp array to DslMat of sundials
         ndarray2DlsMatd(Jac, jac_tmp)
@@ -83,18 +84,18 @@ cdef class CV_data:
 cdef class CVODE:
 
     def __cinit__(self, Rfn, **options):
-        """ 
-        Initialize the CVODE Solver and it's default values 
+        """
+        Initialize the CVODE Solver and it's default values
 
         Input:
             Rfn     - right-hand-side function
             options - additional options for initialization
-            
+
         """
-        
+
         default_values = {
             'implementation': 'serial',
-            'lmm_type': 'BDF', 
+            'lmm_type': 'BDF',
             'iter_type': 'NEWTON',
             'rtol': 1e-6, 'atol': 1e-12,
             'linsolver': 'dense',
@@ -123,9 +124,9 @@ cdef class CVODE:
         self.initialized = False
 
     def set_options(self, **options):
-        """ 
+        """
         Reads the options list and assigns values for the solver.
-        
+
         All options list:
             'implementation':
                 Values: 'serial' (= default), 'parallel'
@@ -148,74 +149,86 @@ cdef class CVODE:
                 Description:
                     See 'lmm_type'.
             'rfn':
-                Values: function of class RhsFunction or a python function with signature (t, y, yp) or
-                        (t, y, yp, userdata)
+                Values: function of class RhsFunction or a python function
+                        with signature (t, y, yp) or (t, y, yp, userdata)
                 Description:
-                    Defines the right-hand-side function (which has to be a subclass of RhsFunction class, or a normal python function with signature (t, y, yp) or (t, y, yp, userdata)).
-                    This function takes as input arguments current time t, current value of y, and yp must be used as output numpy array of returned values of \dot{y} at t
+                    Defines the right-hand-side function (which has to be
+                    a subclass of RhsFunction class, or a normal python
+                    function with signature (t, y, yp) or (t, y, yp, userdata)).
+                    This function takes as input arguments current time t,
+                    current value of y, and yp must be used as output numpy
+                    array of returned values of \dot{y} at t.
                     Optional userdata. Return value is 0 if successfull.
                     This option is mandatory.
             'jacfn':
                 Values: function of class JacRhsFunction
                 Description:
-                    Defines the jacobian function and has to be a subclass 
+                    Defines the jacobian function and has to be a subclass
                     of JacRhsFunction class or python function.
-                    This function takes as input arguments current time t, 
+                    This function takes as input arguments current time t,
                     current value of y, and a 2D numpy array of returned jacobian
                     and optional userdata. Return value is 0 if successfull.
                     Jacobian is only used for dense or lapackdense linear solver
             'rtol':
                 Values: float,  1e-6 = default
                 Description:
-                    Relative tolerancy. 
+                    Relative tolerancy.
             'atol':
                 Values: float or numpy array of floats,  1e-12 = default
                 Description:
                     Absolute tolerancy
-            'order': 
+            'order':
                 Values: 1, 2, 3, 4, 5 (= default)
                 Description:
                     Specifies the maximum order of the linear multistep method.
             'max_steps':
                 Values: positive integer, 0 = default (uses value of 500)
                 Description:
-                    Maximum number of (internally defined) steps allowed during one call to the solver.
+                    Maximum number of (internally defined) steps allowed
+                    during one call to the solver.
             'max_step_size':
                 Values: non-negative float, 0.0 = default
                 Description:
-                    Restricts the maximal (absolute) internal step value taken by the solver. The value of 0.0 uses
+                    Restricts the maximal (absolute) internal step value taken
+                    by the solver. The value of 0.0 uses
                     no restriction on the maximal step size.
             'min_step_size':
                 Values: non-negative float, 0.0 = default
                 Description:
-                    Restricts the minimal (absolute) internal step value taken by the solver. The value of 0.0 uses
+                    Restricts the minimal (absolute) internal step value taken
+                    by the solver. The value of 0.0 uses
                     no restriction.
             'first_step_size':
                 Values: float , 0.0 = default
                 Description:
-                    Sets the first step size. DAE solver can suffer on the first step, so set this
-                    to circumvent this. The value of 0.0 uses the solver's internal default value.
-            'linsolver': 
-                Values: 'dense' (= default), 'lapackdense', 'band', 'lapackband', 'spgmr', 'spbcg', 'sptfqmr'
+                    Sets the first step size. DAE solver can suffer on the
+                    first step, so set this to circumvent this. The value
+                    of 0.0 uses the solver's internal default value.
+            'linsolver':
+                Values: 'dense' (= default), 'lapackdense', 'band',
+                        'lapackband', 'spgmr', 'spbcg', 'sptfqmr'
                 Description:
                     Specifies used linear solver.
-                    Limitations: Linear solvers for dense and band matrices can be used only
-                                 for serial implementation. For parallel implementation use_relaxation
+                    Limitations: Linear solvers for dense and band matrices
+                                 can be used only for serial implementation.
+                                 For parallel implementation use_relaxation
                                  use lapackdense or lapackband respectively.
             'lband', 'uband':
                 Values: non-negative integer, 0 = default
                 Description:
-                    Specifies the width of the lower band (below the main diagonal) and/or upper diagonal
-                    (above the main diagonal). So the total band width is lband + 1 + uband, where the 1 
-                    stands for the main diagonal (specially, lband = uband = 0 [that is the default] 
+                    Specifies the width of the lower band (below the main
+                    diagonal) and/or upper diagonal (above the main diagonal).
+                    So the total band width is lband + 1 + uband, where the 1
+                    stands for the main diagonal
+                    (specially, lband = uband = 0 [that is the default]
                     denotes the band width = 1, i.e. the main diagonal only).
                     Used only if 'linsolver' is band.
             'maxl':
                 Values: 0 (= default), 1, 2, 3, 4, 5
                 Description:
-                    Dimension of the number of used Krylov subspaces 
+                    Dimension of the number of used Krylov subspaces
                     (used only by 'spgmr', 'spbcg', 'sptfqmr' linsolvers)
-            'tcrit': 
+            'tcrit':
                 Values: float, 0.0 = default
                 Description:
                     Maximum time until which we perform the computations.
@@ -223,19 +236,19 @@ cdef class CVODE:
             'user_data':
                 Values: python object, None = default
                 Description:
-                    Additional data that are supplied to each call of the residual function 'Rfn' (see below)
-                    and Jacobi function 'jacfn' (if specified one).
+                    Additional data that are supplied to each call of the
+                    residual function 'Rfn' (see below) and Jacobi function
+                    'jacfn' (if specified one).
             'precond_type':
                 default = None
-            'bdf_stability_detection': 
+            'bdf_stability_detection':
                 default = False, only used if lmm_type == 'bdf
-            'max_conv_fails': 
+            'max_conv_fails':
                 default = 0,
-            'max_nonlin_iters': 
+            'max_nonlin_iters':
                 default = 0,
-            'nonlin_conv_coef': 
+            'nonlin_conv_coef':
                 default = 0.
-            
         """
 
         for (key, value) in options.items():
@@ -256,14 +269,14 @@ cdef class CVODE:
         # how to unset tcrit??
         CVodeSetStopTime(cv_mem, <realtype> tcrit)
 
-    cpdef _init_step(self, DTYPE_t t0, 
+    cpdef _init_step(self, DTYPE_t t0,
                      np.ndarray[DTYPE_t, ndim=1] y0):
 
         cdef dict opts = self.options
 
         lmm_type  = opts['lmm_type'].lower()
         iter_type = opts['iter_type'].lower()
-        
+
         if lmm_type == 'bdf':
             lmm = CV_BDF
         elif lmm_type == 'adams':
@@ -276,9 +289,11 @@ cdef class CVODE:
         elif iter_type == 'newton':
             itert = CV_NEWTON
         else:
-            raise ValueError('CVODE:init: Unrecognized iter_type: %s' % iter_type)
- 
-        self.parallel_implementation = (opts['implementation'].lower() == 'parallel')
+            raise ValueError('CVODE:init: Unrecognized iter_type: %s'
+                             % iter_type)
+
+        self.parallel_implementation = \
+          (opts['implementation'].lower() == 'parallel')
         if self.parallel_implementation:
             raise ValueError('Error: Parallel implementation not implemented !')
         cdef long int N
@@ -310,8 +325,9 @@ cdef class CVODE:
                 CVodeFree(&cv_mem)
             cv_mem = CVodeCreate(lmm, itert)
             if cv_mem is NULL:
-                raise MemoryError('CVodeCreate:MemoryError: Could not create cv_mem object')
-            
+                raise MemoryError('CVodeCreate:MemoryError: Could not create '
+                                  'cv_mem object')
+
             self._cv_mem = cv_mem
             flag = CVodeInit(cv_mem, _rhsfn,  <realtype> t0, self.y0)
         elif self.N == N:
@@ -372,7 +388,7 @@ cdef class CVODE:
                 atol = N_VMake_Serial(N, <realtype *> np_atol.data)
                 flag = CVodeSVtolerances(cv_mem, <realtype> opts['rtol'], atol)
         #TODO: implement CVFWtolerances(cv_mem, efun)
-        
+
         if flag == CV_ILL_INPUT:
             raise ValueError('CVodeStolerances: negative atol or rtol value.')
 
@@ -391,7 +407,8 @@ cdef class CVODE:
         if opts['max_step_size'] > 0.:
             flag = CVodeSetMaxStep(cv_mem, <realtype> opts['max_step_size'])
             if flag == CV_ILL_INPUT:
-                raise ValueError('CVodeSetMaxStep: max_step_size is negative or smaller than min_step_size.')
+                raise ValueError('CVodeSetMaxStep: max_step_size is negative '
+                                 'or smaller than min_step_size.')
         if opts['tcrit'] > 0.:
             CVodeSetStopTime(cv_mem, <realtype> opts['tcrit'])
         if opts['max_nonlin_iters'] > 0:
@@ -403,12 +420,12 @@ cdef class CVODE:
 
         # Linsolver
         if iter_type == 'newton':
-                
+
             linsolver = opts['linsolver'].lower()
 
             if linsolver == 'dense':
                 if self.parallel_implementation:
-                    raise ValueError('Linear solver for dense matrices can be' 
+                    raise ValueError('Linear solver for dense matrices can be'
                                      'used only for serial implementation. Use'
                                      ' ''lapackdense'' for parallel implementation.')
                 else:
@@ -431,7 +448,7 @@ cdef class CVODE:
                                      'only for serial implementation. Use ''lapackband'' '
                                      'instead for parallel implementation.')
                 else:
-                    flag = CVBand(cv_mem, N, <int> opts['uband'], 
+                    flag = CVBand(cv_mem, N, <int> opts['uband'],
                                              <int> opts['lband'])
                     if flag == CVDLS_ILL_INPUT:
                         raise ValueError('CVBand solver is not compatible'
@@ -440,7 +457,7 @@ cdef class CVODE:
                     elif flag == CVDLS_MEM_FAIL:
                         raise MemoryError('CVBand memory allocation error.')
             elif linsolver == 'lapackband':
-                flag = CVLapackBand(cv_mem, N, <int> opts['uband'], 
+                flag = CVLapackBand(cv_mem, N, <int> opts['uband'],
                                                <int> opts['lband'])
                 if flag == CVDLS_ILL_INPUT:
                     raise ValueError('CVLapackBand solver is not compatible'
@@ -455,7 +472,7 @@ cdef class CVODE:
                                          ' the current nvector implementation.')
                 elif flag == CVDIAG_MEM_FAIL:
                         raise MemoryError('CVDiag memory allocation error.')
-            elif ((linsolver == 'spgmr') or (linsolver == 'spbcg') 
+            elif ((linsolver == 'spgmr') or (linsolver == 'spbcg')
                   or (linsolver == 'sptfqmr')):
                 precond_type = opts['precond_type'].lower()
                 if precond_type == 'none':
@@ -499,15 +516,15 @@ cdef class CVODE:
         #TODO: determine hook_fn type
 
         return self._solve(np_tspan, np_y0, hook_fn)
-            
-    cpdef _solve(self, np.ndarray[DTYPE_t, ndim=1] tspan, 
+
+    cpdef _solve(self, np.ndarray[DTYPE_t, ndim=1] tspan,
                  np.ndarray[DTYPE_t, ndim=1] y0, hook_fn = None):
 
         cdef np.ndarray[DTYPE_t, ndim=1] t_retn
         cdef np.ndarray[DTYPE_t, ndim=2] y_retn
         t_retn  = np.empty(np.shape(tspan), float)
         y_retn  = np.empty([np.alen(tspan), np.alen(y0)], float)
-            
+
         self._init_step(tspan[0], y0)
         t_retn[0] = tspan[0]
         y_retn[0, :] = y0
@@ -521,7 +538,7 @@ cdef class CVODE:
         cdef N_Vector y  = self.y
 
         y_last   = np.empty(np.shape(y0), float)
-                
+
         if hook_fn:
             for idx in np.arange(np.alen(tspan))[1:]:
                 t = tspan[idx]
@@ -531,8 +548,8 @@ cdef class CVODE:
                                  CV_ONE_STEP)
 
                     nv_s2ndarray(y,  y_last)
-                    
-                    if ((flag < 0) or 
+
+                    if ((flag < 0) or
                         hook_fn(t_out, y_last, self.aux_data.user_data) != 0):
                         if flag < 0:
                             print('Error occured. See returned flag '
@@ -542,12 +559,12 @@ cdef class CVODE:
 
                         t_retn  = t_retn[0:idx]
                         y_retn  = y_retn[0:idx, :]
-                
+
                         return flag, t_retn, y_retn, t_out, y_last
 
                     if t_out > t: break
 
-                t_retn[idx]    = t_out 
+                t_retn[idx]    = t_out
                 y_retn[idx, :] = y_last
         else:
             for idx in np.arange(np.alen(tspan))[1:]:
@@ -562,12 +579,12 @@ cdef class CVODE:
                           'variable and CVode documentation.')
                     t_retn  = t_retn[0:idx]
                     y_retn  = y_retn[0:idx, :]
-                
+
                     return flag, t_retn, y_retn, t_out, y_last
 
                 t_retn[idx]    = t_out
                 y_retn[idx, :] = y_last
-                    
+
         return flag, t_retn, y_retn, None, None
 
     def step(self, DTYPE_t t, np.ndarray[DTYPE_t, ndim=1] y_retn):
@@ -575,7 +592,7 @@ cdef class CVODE:
         Method for calling successive next step of the CVODE solver to allow
         more precise control over the solver. The 'init_step' method has to
         be called before the 'step' method.
-        
+
         Input:
             t - if t>0.0 then integration is performed until this time
                          and results at this time are returned in y_retn
@@ -585,11 +602,12 @@ cdef class CVODE:
                      value will be stored  (needs to be preallocated)
         Return values:
             flag  - status of the computation (successful or error occured)
-            t_out - time, where the solver stopped (when no error occured, t_out == t)
+            t_out - time, where the solver stopped
+                    (when no error occured, t_out == t)
         """
         if not self.initialized:
-            raise ValueError('Method ''init_step'' has to be called prior to the'
-                             'first call of ''step'' method.')
+            raise ValueError('Method ''init_step'' has to be called prior to '
+                             'the first call of ''step'' method.')
 
         cdef N_Vector y  = self.y
         cdef realtype t_out
