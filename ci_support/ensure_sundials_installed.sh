aa662e561f455d0b286a528cd310b69d449db22e
James Tocknell <aragilar@gmail.com>
2016-03-07 18:04:14 +1100

    Add support for tox and travis ci

---
+++ ci_support/ensure_sundials_installed.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export SUNDIALS_DIR=$HOME/sundials
+SUNDIALS_LIBDIR=$SUNDIALS_DIR/lib
+SUNDIALS_INCLUDEDIR=$SUNDIALS_DIR/include
+
+if [ ! -d "$SUNDIALS_LIBDIR" ]; then
+    mkdir -p $SUNDIALS_DIR
+    echo "Installing sundials"
+    ./ci_support/install_sundials.sh
+else
+    echo "Using cached sundials"
+fi
+
+export LD_LIBRARY_PATH=$SUNDIALS_LIBDIR:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$SUNDIALS_LIBDIR:$LIBRARY_PATH
+export CPATH=$SUNDIALS_INCLUDEDIR:$CPATH
