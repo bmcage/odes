d8d0fad09febfd1efb2758d307c12704bdc01d7d
James Tocknell <aragilar@gmail.com>
2018-03-24 20:24:17 +1100

    Skip running doctr as needed so CI doesn't always fail

--- upload_api_docs.sh
+++ upload_api_docs.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" = "bmcage/odes" -o "$DOCTR_DEPLOY" ]; then
+    deploy="true"
+else
+    deploy="false"
+fi
+
+
 if [ -z "$TRAVIS_TAG" ]; then
     DEPLOY_DIR=dev;
 else
@@ -8,4 +17,8 @@ fi
 
 make -C apidocs html
 
-doctr deploy --no-require-master --build-tags --command "$VIRTUAL_ENV/bin/python build_index.py" --built-docs apidocs/_build/html $DEPLOY_DIR
+if [ $deploy = "true" ]; then
+    doctr deploy --no-require-master --build-tags --command "$VIRTUAL_ENV/bin/python build_index.py" --built-docs apidocs/_build/html $DEPLOY_DIR
+else
+    echo 'Not deploying docs, set DOCTR_DEPLOY to do deployment of docs'
+fi
